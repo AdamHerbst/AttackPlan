0d2d6d728ea25912edae0eb02cae653bf23a4b74
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-02-16 14:14:30 -0500

    Create logindb.php

---
+++ refactored/logindb.php
@@ -0,0 +1,25 @@
+<?php
+
+// Include the file that configures the DB.
+include 'includes/db-config.inc.php';
+
+?>
+
+<html>
+<body>
+
+<?php
+
+
+    // $db = new UserDAO($connection);
+    // $user = $db->
+    // echo '<H3> Record 30</h3>';
+    // echo $user->getUsername();
+ 
+    
+
+
+?>
+
+</body>
+</html>
